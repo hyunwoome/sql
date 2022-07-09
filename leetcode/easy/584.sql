435517a24ffbb584e1a7db99cf9eab02a0cfe84d
hyunwoome <hyunwoome@icloud.com>
2022-07-09 13:10:19 +0900

    solve: LeetCode 584

---
+++ leetcode/easy/584.sql
@@ -0,0 +1,23 @@
+USE leetcode;
+CREATE TABLE IF NOT EXISTS Customer
+(
+    id         int,
+    name       varchar(25),
+    referee_id int
+);
+TRUNCATE TABLE Customer;
+INSERT INTO Customer (id, name, referee_id)
+VALUES ('1', 'Will', NULL)
+INSERT INTO Customer (id, name, referee_id)
+VALUES ('2', 'Jane', NULL)
+INSERT INTO Customer (id, name, referee_id)
+VALUES ('3', 'Alex', '2')
+INSERT INTO Customer (id, name, referee_id)
+VALUES ('4', 'Bill', NULL)
+INSERT INTO Customer (id, name, referee_id)
+VALUES ('5', 'Zack', '1')
+INSERT INTO Customer (id, name, referee_id)
+VALUES ('6', 'Mark', '2');
+
+SELECT name FROM Customer
+WHERE referee_id IS NULL OR referee_id != 2;
