fefa13f9d785db14e877e6b1117fbff93992876a
hyunwoome <hyunwoome@icloud.com>
2022-07-13 09:14:33 +0900

    solve: LeetCode 595

---
+++ leetcode/easy/595.sql
@@ -0,0 +1,26 @@
+USE leetcode;
+CREATE TABLE IF NOT EXISTS World
+(
+    name       varchar(255),
+    continent  varchar(255),
+    area       int,
+    population int,
+    gdp        bigint
+);
+TRUNCATE TABLE World;
+INSERT INTO World (name, continent, area, population, gdp)
+VALUES ('Afghanistan', 'Asia', '652230', '25500100', '20343000000');
+INSERT INTO World (name, continent, area, population, gdp)
+VALUES ('Albania', 'Europe', '28748', '2831741', '12960000000');
+INSERT INTO World (name, continent, area, population, gdp)
+VALUES ('Algeria', 'Africa', '2381741', '37100000', '188681000000');
+INSERT INTO World (name, continent, area, population, gdp)
+VALUES ('Andorra', 'Europe', '468', '78115', '3712000000');
+INSERT INTO World (name, continent, area, population, gdp)
+VALUES ('Angola', 'Africa', '1246700', '20609294', '100990000000');
+
+SELECT name,
+       population,
+       area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000;
